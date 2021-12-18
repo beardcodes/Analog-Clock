8cdffd1c88083cf1d8459d0598363731fde94bf0
molessom-8 <81950204+molessom-8@users.noreply.github.com>
2021-12-18 11:01:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const minEl = document.querySelector('.min');
+const secEl = document.querySelector('.sec');
+const hourEl = document.querySelector('.hour');
+
+
+
+setInterval(() => {
+    const date = new Date;
+    const secDeg = date.getSeconds() / 60 * 360 - 90;
+    const minDeg = date.getMinutes() / 60 * 360 - 90;
+    const hourDeg = date.getHours() / 12 * 360 - 90;
+    secEl.style.transform = `rotate(${secDeg}deg)`;
+    minEl.style.transform = `rotate(${minDeg}deg)`;
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+}, 1000)
+
